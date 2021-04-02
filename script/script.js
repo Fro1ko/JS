7820d1288555d8d038921621340c4abe42607ec4
Vlad Frolov <frolko95@mail.ru>
2021-04-03 02:53:48 +0400

    lesson03

--- script/script.js
+++ script/script.js
@@ -1,9 +1,23 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 30000;
+let income = "фриланс";
+let addExpenses = "Бензин, Газ, Интернет, Вода, Такси, Еда";
+let deposit = true;
+let mission = 1000000;
+let period = 6;
 
-alert("Любой текст");
-console.log("какой-то текст в консоле");
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + mission + " рублей");
+
+console.log(addExpenses.toLowerCase());
+
+console.log(addExpenses.split(", "));
+
+let budgetDay = money / 30;
+
+console.log(budgetDay );
