825ba23af762015073f2cd04cc05af30251ecd69
Vlad Frolov <frolko95@mail.ru>
2021-04-21 22:45:18 +0400

    lesson07

--- script/script.js
+++ script/script.js
@@ -5,84 +5,80 @@ let isNumber = function (n) {
 };
 
 let money;
-let income = "фриланс";
-let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-let deposit = confirm("Есть ли у вас депозит в банке?");
-let mission = 1000000;
-let period = 6;
-
 do {
   money = prompt("Ваш месячный доход?");
 } while (!isNumber(money));
 
+let appData = {
+  budget: +money,
+  budgetDay: 200,
+  budgetMonth: 150,
+  expensesMonth: 0,
+  income: {},
+  addIncome: [],
+  expenses: {},
+  addExpenses: [],
+  deposit: false,
+  mission: 1000000,
+  period: 6,
+  asking: function() {
+    let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
+        sun = 0,
+        n;
+    appData.addExpenses = addExpenses.toLowerCase().split(",");
+    appData.deposit = confirm("Есть ли у вас депозит в банке?");
 
-function showTypeOf(type) {
-  console.log(type);
+    for (let i = 0; i < 2; i++) {
+      appData.expenses[prompt('Введите обязательную статью расходов?')] = (() => {
+        let n = 0;
+        do {
+            n = prompt('Во сколько это обойдется?');
+        } while (!isNumber(n));
+        return +n;
+    })();
+    }
+  },
+  getExpensesMonth: function() {
+    appData.expensesMonth = 0; 
+    for (let el in appData.expenses) {
+      appData.expensesMonth += appData.expenses[el];
 }
-showTypeOf(typeof(money));
-showTypeOf(typeof(income));
-showTypeOf(typeof(deposit));
 
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(","));
+  },
 
-let expenses = [];
-
-function getExpensesMonth() {
-  let sun = 0,
-      n;
-
-  for (let i = 0; i < 2; i++) {
-    
-    expenses[i] = prompt("Введите обязательную статью расходов?");
-    
-    
-    do {
-      n = prompt("Во сколько это обойдется?");
-    } while (!isNumber(n));
-    sun += +n;
-    
+  getBudget: function() {
+    if (!appData.budget) {
+    appData.budget = 0;
   }
-  return sun;
-}
-
-let expensesAmount = getExpensesMonth();
-
-console.log(expensesAmount); 
-
-function getAccumulatedMonth(monthMoney, expenses) {
-  if (!expenses) {
-    expenses = 0;
-  }
-  return monthMoney - expenses;
-}
-let accumulatedMonth = getAccumulatedMonth(money, expensesAmount);
-console.log(accumulatedMonth);
-
-function getTargetMonth(targetMission, budjetMonth) {
-  return Math.ceil(targetMission / budjetMonth);
-}
-console.log(getTargetMonth(mission, accumulatedMonth));
-
-let targetMonth = getTargetMonth(mission, accumulatedMonth);
-
-let budgetDay = accumulatedMonth / 30;
-console.log(budgetDay);
-
-(targetMonth >= 0) ? 
-console.log("Цель будет достигнута за " + targetMonth + " месяца") : 
-console.log("Цель не будет достигнута");
-
-
-function getStatusIncome() {
-  if (budgetDay >= 1200) {
+  appData.budgetMonth = appData.budget - appData.expensesMonth;
+  appData.budgetDay = appData.budgetMonth / 30;
+  },
+  getTargetMonth: function() {
+    return Math.ceil(appData.mission / appData.budgetMonth);
+  },
+  getStatusIncome: function() {
+    
+  if (appData.budgetDay >= 1200) {
     console.log("У вас высокий уровень дохода");
-  } else if (1200 > budgetDay && budgetDay >= 600) {
+  } else if (1200 > appData.budgetDay && appData.budgetDay >= 600) {
     console.log("У вас средний уровень дохода");
-  } else if (600 > budgetDay && budgetDay >= 0) {
+  } else if (600 > appData.budgetDay && appData.budgetDay >= 0) {
     console.log("К сожалению у вас уровень дохода ниже среднего");
   } else {
     console.log("Что то пошло не так");
   }
+  }
+};
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
+console.log(appData.expensesMonth);
+let targetMonth = appData.getTargetMonth();
+(targetMonth >= 0) ? 
+console.log("Цель будет достигнута за " + targetMonth + " месяца") : console.log("Цель не будет достигнута");
+appData.getStatusIncome();
+
+console.log('Наша программа включает в себя данные:');
+for (let el in appData) {
+    console.log(el, appData[el]);
 }
-getStatusIncome();
