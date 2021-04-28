5ceef8b42fa5f3c1a3438ad6ad96c65127328764
Vlad Frolov <frolko95@mail.ru>
2021-04-28 22:02:06 +0400

    lesson09

---
+++ script/script.js
@@ -0,0 +1,42 @@
+let start1 = document.getElementById("start");
+let incomePlus = document.getElementsByTagName("button")[0];
+let expensesPlus = document.getElementsByTagName("button")[1];
+let checkbox = document.querySelector("#deposit-check");
+let allIncomeItem = document.querySelectorAll(".additional_income-item");
+let budgetDay = document.getElementsByClassName("budget_day-value");
+let expensesMonth = document.getElementsByClassName("expenses_month-value");
+let additionalIncome = document.getElementsByClassName("additional_income-value");
+let additionalExpenses = document.getElementsByClassName("additional_expenses-value");
+let incomePeriod = document.getElementsByClassName("income_period-value");
+let targetMonth = document.getElementsByClassName("target_month-value");
+
+let salaryAmount = document.querySelector(".salary-amount");
+let incomeItems = document.querySelector(".income-items");
+let incomeAmount = document.querySelector(".income-amount");
+let expensesItems = document.querySelector(".expenses-items");
+let expensesAmount = document.querySelector(".expenses-amount");
+let additionalExpensesItem = document.querySelector(".additional_expenses-item");
+let targetAmount = document.querySelector(".target-amount");
+let periodSelect = document.querySelector('.period-select');
+
+
+console.log(start1);
+console.log(incomePlus);
+console.log(expensesPlus);
+console.log(checkbox);
+console.log(allIncomeItem);
+console.log(budgetDay);
+console.log(expensesMonth);
+console.log(additionalIncome);
+console.log(additionalExpenses);
+console.log(incomePeriod);
+console.log(targetMonth);
+console.log(123321);
+console.log(salaryAmount);
+console.log(incomeItems.children[0]);
+console.log(incomeAmount);
+console.log(expensesItems.children[0]);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(targetAmount);
+console.log(periodSelect);
